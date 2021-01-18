b7596204abd82a5e3c30ed13ed2046b24e4395a6
nathan-young1 <okoriejonathan123@gmail.com>
2021-01-18 03:01:01 +0100

    fixed the token issue

--- functions/index.js
+++ functions/index.js
@@ -3,42 +3,44 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 
-exports.fileCreated = functions.storage.object().onFinalize(async (object,context) => {
-  const fileSize = object.size;
-  const uid = context.auth.uid;
-  console.log(`${fileSize} ${uid}`);
-  const currentSpace = context.auth.token.StorageLeft;
+exports.fileCreated = functions.storage.object('user/{userUid}/{collection}').onFinalize(async (object,context) => {
+  console.log('joe the joe');
+  console.log(context.params.userUid);
+  console.log(context.auth.uid());
+  const fileSize = Number(object.size);
+  const uid = String(context.auth.uid);
+  console.log(`${fileSize} ${context.auth.uid}`);
+  const currentSpace = Number(context.auth.token.storageLeft);
   const storageSpaceLeft = currentSpace - fileSize;
-  const customClaims = {
-    StorageLeft: storageSpaceLeft
-    };
 
-  return await admin.auth().setCustomUserClaims(uid,customClaims);
+  return await admin.auth().setCustomUserClaims(uid,{storageLeft: storageSpaceLeft});
   });
 
 
   exports.fileDeleted = functions.storage.object().onDelete(async (object,context)=> {
-    const fileSize = object.size;
-    const uid = context.auth.uid;
+    const fileSize = Number(object.size);
+    const uid = String(context.auth.uid);
     console.log(`${fileSize} ${uid}`);
-    const currentSpace = context.auth.token.StorageLeft;
+    const currentSpace = Number(context.auth.token.storageLeft);
     const storageSpaceLeft = currentSpace + fileSize;
-
-    const customClaims = {
-      StorageLeft: storageSpaceLeft
-      };
     
-  return await admin.auth().setCustomUserClaims(uid,customClaims);
+  return await admin.auth().setCustomUserClaims(uid,{storageLeft: storageSpaceLeft});
   });
 
 
   exports.userCreate = functions.auth.user().onCreate((user) => {
-    
-  const customClaims = {
-    StorageLeft: 2 *1024 *1024 *1024
-    };
-    
-  return admin.auth().setCustomUserClaims(user.uid,customClaims);
+    let uid = user.uid;
+    console.log(user.uid);
+    admin.auth().setCustomUserClaims(uid,{storageLeft: 2 *1024 *1024 *1024}).then(async (full,reject)=>{
+      console.log((await admin.auth().getUser(uid)).customClaims);
+      console.log('starteddsad');
+      console.log('startefdsrrr');
+      return 'finished creating user';
+    }
+      ).catch(e=>{
+        console.log(e);
+      });
+  return 'finished creating user';
   });
 
 
